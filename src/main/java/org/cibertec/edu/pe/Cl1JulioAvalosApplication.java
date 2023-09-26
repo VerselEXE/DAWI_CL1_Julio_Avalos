72653898cf84aa5fb22214373fd12c95002d6429
VerselEXE <I202123660@cibertec.edu.pe>
2023-09-26 15:25:59 -0500

    repositorio inicial subido

---
+++ src/main/java/org/cibertec/edu/pe/Cl1JulioAvalosApplication.java
@@ -0,0 +1,13 @@
+package org.cibertec.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1JulioAvalosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1JulioAvalosApplication.class, args);
+	}
+
+}
